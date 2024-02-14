6117fa6fa6dfbad12502e877da1dfd0dd93059c3
MenlyCSE <MenlyCSE@gmail.com>
2024-02-13 19:19:36 -0500

    update

--- CalorieCounter/script.js
+++ CalorieCounter/script.js
@@ -7,6 +7,18 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    str.replace(regex, "");
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
 }
