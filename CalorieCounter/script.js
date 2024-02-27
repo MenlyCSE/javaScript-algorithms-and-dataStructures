cf714f0fe5e47e0dcf789955376a655f11443b96
MenlyCSE <MenlyCSE@gmail.com>
2024-02-27 18:53:58 -0500

    update

--- CalorieCounter/script.js
+++ CalorieCounter/script.js
@@ -63,9 +63,8 @@ function calculateCalories(e) {
   <p>${consumedCalories} Calories Consumed</p>
   <p>${exerciseCalories} Calories Burned</p>
   `;
+
   output.classList.remove('hide');
-
-
 }
 
 function getCaloriesFromInputs(list) {
@@ -85,4 +84,18 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+  for (const container of inputContainers) {
+    container.innerHTML = '';
+  }
+
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide');
+}
+
 addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener('click', clearForm);
