035127b8652c32b28514b8341f31292d3ae0a7df
MenlyCSE <MenlyCSE@gmail.com>
2024-03-10 19:18:48 -0400

    Update

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -122,7 +122,7 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () =>{
+const playPreviousSong = () => {
    if (userData?.currentSong === null) return;
    else {
     const currentSongIndex = getCurrentSongIndex();
@@ -134,8 +134,16 @@ const playPreviousSong = () =>{
 
 const shuffle = () => {
   userData?.songs.sort(() => Math.random() - 0.5);
-  
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
 };
+
+
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
@@ -206,6 +214,8 @@ pauseButton.addEventListener("click",  pauseSong);
 nextButton.addEventListener("click", playNextSong);
 
 previousButton.addEventListener("click", playPreviousSong);
+
+shuffleButton.addEventListener("click", shuffle);
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
