7f22080168c1920efdc7627475984ae5af5db78c
MenlyCSE <MenlyCSE@gmail.com>
2024-03-28 19:58:43 -0400

    Update

--- FootballTeamCards/script.js
+++ FootballTeamCards/script.js
@@ -175,5 +175,24 @@ Object.freeze(myFavoriteFootballTeam);
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
 
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
 
 
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+      `
+        <div class="player-card">
+          <h2>${name} ${isCaptain ? ("Captain") : ""} </h2>
+        </div>
+      `;
+
+    }
+  );
+
+
+};
