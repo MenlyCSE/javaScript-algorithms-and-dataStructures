c947561766216c44f81c4446ec1c332914b8fdb6
MenlyCSE <MenlyCSE@gmail.com>
2024-04-04 20:05:42 -0400

    Update

--- DecimalToBinaryConverter/script.js
+++ DecimalToBinaryConverter/script.js
@@ -2,12 +2,21 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+
+
 const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
   console.log(numberInput.value);
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 
 numberInput.addEventListener("keydown", (e) => {
-  console.log(e);
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
