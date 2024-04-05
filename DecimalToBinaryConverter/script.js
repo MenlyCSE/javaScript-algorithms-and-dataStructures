abbdb72d46b0c60f24a7108087213dae2ab5d01f
MenlyCSE <MenlyCSE@gmail.com>
2024-04-05 19:48:50 -0400

    Update

--- DecimalToBinaryConverter/script.js
+++ DecimalToBinaryConverter/script.js
@@ -2,7 +2,23 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
 
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+
+};
 
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
@@ -10,7 +26,8 @@ const checkUserInput = () => {
     return;
   }
 
-  console.log(numberInput.value);
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
