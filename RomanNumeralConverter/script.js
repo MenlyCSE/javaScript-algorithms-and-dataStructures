71b6789a83096c786e16b70335c492593731ea7e
MenlyCSE <MenlyCSE@gmail.com>
2024-04-18 19:10:33 -0400

    Update

--- RomanNumeralConverter/script.js
+++ RomanNumeralConverter/script.js
@@ -1,11 +1,22 @@
 const convertBtn = document.getElementById('convert-btn');
-const input = document.getElementById('number');
+const userInput = document.getElementById('number');
 const output = document.getElementById('output')
 
 
-const inputError = () => {
-  if (input.value === "") {
+const inputChecker = () => {
+  if (userInput.value === "") {
+    output.innerHTML = "Please enter a valid number";
+  } else if (userInput.value < 1) {
     output.innerHTML = "Please enter a number greater than or equal to 1";
+  } else if (userInput.value >= 4000) {
+    output.innerHTML = "Please enter a number less than or equal to 3999";
   }
 }
-convertBtn.addEventListener('click', inputError)
+
+
+convertBtn.addEventListener('click', inputChecker)
+userInput.addEventListener('keydown', (e) => {
+  if (e.key === "Enter") {
+    inputChecker();
+  }
+})
