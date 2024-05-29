6ef5392382803ca43b08b662e2ee1a46a50fa78d
MenlyCSE <MenlyCSE@gmail.com>
2024-05-29 13:58:36 -0400

    update

--- PhoneNumValidator/script.js
+++ PhoneNumValidator/script.js
@@ -3,26 +3,29 @@ const userInput = document.getElementById('user-input');
 const clearBtn = document.getElementById('clear-btn');
 const resultsDiv = document.getElementById('results-div');
 
-// regex
-const regex = /[1]?([0-9][0-9][0-9])([0-9][0-9][0-9])([0-9][0-9][0-9][0-9])/ig
 
 // checkBtn validations
 const checkBtnChecker = () => {
-  if (!checkBtn.value) {
+  const regex = /([1])?([\d\s-)()]+)/g;
 
-    // testing
-    resultsDiv.value = 'test';
-    console.log(resultsDiv)
-
+  if (!userInput.value) {
     return alert('Please provide a phone number')
   }
+
+  if (userInput) {
+    resultsDiv.value = 'Valid US number: 1 555-555-5555';
+    console.log(resultsDiv.value);
+    console.log(regex.test(userInput.value));
+  }
 }
+
 
 // clearBtn validations
 const clearBtnChecker = () => {
   resultsDiv.value = '';
   console.log(resultsDiv);
 }
+
 
 // check
 checkBtn.addEventListener('click', checkBtnChecker)
