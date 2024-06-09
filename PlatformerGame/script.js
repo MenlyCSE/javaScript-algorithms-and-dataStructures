f85d005d2da25846025d4832176696bad8533e28
MenlyCSE <MenlyCSE@gmail.com>
2024-06-09 16:46:55 -0400

    Update script.js

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -36,12 +36,52 @@ class Player {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
-
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-    
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
     }
 
+    if (this.position.x < this.width) {
+      this.position.x = this.width;
+    }
+
+    if (this.position.x >= canvas.width - 2 * this.width) {
+      this.position.x = canvas.width - 2 * this.width;
+    }
+  }
+}
+
+const player = new Player();
+
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  if (keys.rightKey.pressed === true && this.position.x < proportionalSize(400)) {
 
   }
-
 }
+
+
+const keys = {
+  rightKey: {
+    pressed: false
+  },
+  leftKey: {
+    pressed: false
+  }
+};
+
+const startGame = () => {
+  canvas.style.display = "block";
+  startScreen.style.display = "none";
+  player.draw();
+}
+
+startBtn.addEventListener("click", startGame);
