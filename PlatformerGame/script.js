28e7b9d10d2d0108a1f4de78fac530826ee7c5a5
MenlyCSE <MenlyCSE@gmail.com>
2024-06-09 14:40:46 -0400

    Update script.js

--- PlatformerGame/script.js
+++ PlatformerGame/script.js
@@ -13,14 +13,35 @@ const proportionalSize = (size) => {
   return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 }
 
-
 class Player {
   constructor() {
     this.position = {
       x: proportionalSize(10),
       y: proportionalSize(400),
     };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = "#99c9ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+    
+    }
+
 
   }
-}
 
+}
