6775fd8b9be9a2beddf2bb2d81fdb3784cf3f452
MenlyCSE <MenlyCSE@gmail.com>
2024-07-15 21:45:06 -0400

    beautify

--- PokemonSearchApp/script.js
+++ PokemonSearchApp/script.js
@@ -7,49 +7,38 @@ const pokemonHeight = document.getElementById('height');
 const pokemonTypes = document.getElementById('types');
 const spriteContainer = document.getElementById('sprite-container');
 
-const validator = () => {
-  const pokeURL = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
-  const idSearch = parseFloat(searchInput.value) - 1;
-  
 
-  // get info from api
+const validator = () => {
+  const idSearch = parseFloat(searchInput.value) - 1;
+  const nameSearch = (searchInput.value).toLowerCase();
+  pokemonTypes.innerHTML = ``; // resets values
+  const pokeURL = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${idSearch + 1 || nameSearch}`;
+
+  // retrieve data from api
   fetch(pokeURL)
     .then(response => response.json())
     .then(data => {
 
       // destructs
-      const { results } = data;
-      const { url, name, id } = results[idSearch];
+      const { height, id, name, sprites, stats, types, weight } = data;
 
       // assign values
       pokemonName.innerHTML = `${name.toUpperCase()}`;
       pokemonId.innerHTML = `#${id}`;
+      pokemonWeight.textContent = `Weight: ${weight}`;
+      pokemonHeight.textContent = `Height: ${height}`;
+      spriteContainer.innerHTML = `<img id="sprite" src="${sprites.front_default}" alt="${data.name} front default">`;
 
+      // loop and append values
+      types.forEach((object) => {
+        pokemonTypes.innerHTML += `<span class="type ${object.type.name}">${object.type.name}</span>`;
+      });
 
-      // get info from api
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-
-          // destructs
-          const { weight, height, types, stats, sprites } = data;
-
-          // assign values
-          pokemonWeight.textContent = `Weight: ${weight}`;
-          pokemonHeight.textContent = `Height: ${height}`;
-          spriteContainer.innerHTML = `<img id="sprite" src="${sprites.front_default}" alt="${data.name} front default">`;
-
-          types.forEach((object) => {
-            pokemonTypes.innerHTML += `<span class="type ${object.type.name}">${object.type.name}</span>`;
-          });
-
-          stats.forEach((index) => {
-            document.getElementById(`${index.stat.name}`).textContent = `${index.base_stat}`;
-          });
-        })
-        .catch(error => console.log("Inner:", error));
+      stats.forEach((index) => {
+        document.getElementById(`${index.stat.name}`).textContent = `${index.base_stat}`;
+      });
     })
-    .catch(error => console.log(error));
+    .catch(alert("Pokémon not found"));
 };
 
 
